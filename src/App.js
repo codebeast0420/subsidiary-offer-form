08267f0a7a98d3df5568640e6852d1cc31423d46
John <codebeast0420@gmail.com>
2024-08-01 17:11:27 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -338,10 +338,10 @@ function App() {
                   name='sector'
                   onChange={handleChange}
                 >
-                  <MenuItem key='none' value={'None'}>None</MenuItem>
                   {sectors[industry].map((s, index) => (
                     <MenuItem key={index} value={s}>{s}</MenuItem>
                   ))}
+                  <MenuItem key='none' value={'None'}>None</MenuItem>
                 </Select>
               </FormControl>
             </div>
